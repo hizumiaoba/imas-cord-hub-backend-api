44b31836cf1be18b8aa785faab88766ff1981a41
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-29 22:34:57 +0900

    add experimental servers endpoint

---
+++ api/servers.ts
@@ -0,0 +1,7 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  return res.json({
+    message: "This is the /servers endpoint!"
+  });
+}
