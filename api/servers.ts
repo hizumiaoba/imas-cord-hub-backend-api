faf361f2ccbfe7cca7f4273f8829131371667889
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 16:31:16 +0900

    add findbyid endpoint

--- api/servers.ts
+++ api/servers.ts
@@ -20,7 +20,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   return res.status(200).json(result);
 }
 
-type exportType = {
+export type exportType = {
   id: string,
   name: string,
   ip: string,
--- api/servers/inside.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  return res.json({
-    message: "This is the /servers/inside endpoint!"
-  });
-}
