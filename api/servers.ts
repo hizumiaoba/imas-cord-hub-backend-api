da5f083579f0247beed685bcc5f22d0d35e11538
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 15:27:49 +0900

    fix logic

--- api/servers.ts
+++ api/servers.ts
@@ -4,5 +4,7 @@ import clientPromise from "../_utils/mongo";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const client = await clientPromise;
   const db = client.db("imascordhubServer");
-  return res.status(200).json(db.collection("serverData").find({}));
+  const collection = db.collection("serverData");
+  const servers = await collection.find({}).toArray()
+  return res.status(200).json(servers);
 }
