036184f2f247e4c1a21fa1c7d36b669a0ca272a9
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 15:03:04 +0900

    remove firebase related logics

--- api/servers.ts
+++ api/servers.ts
@@ -1,13 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { Firestore, collection, getDocs } from 'firebase/firestore/lite';
-import { createAppContainer, getFirestore } from "./internal/firebaseAppFactory";
-import { FirebaseApp } from "firebase/app";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const firebaseApp: FirebaseApp = createAppContainer();
-  const db: Firestore = getFirestore(firebaseApp);
-  const serverCollection = collection(db, "servers");
-  const serverSnapshot = await getDocs(serverCollection);
-  const servers = serverSnapshot.docs.map(doc => doc.data());
-  return res.json(servers);
+  return res.status(200).json({ message: "This is the /servers endpoint!" });
 }
