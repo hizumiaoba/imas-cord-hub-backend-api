11178e4bb92a3c1a00483597867051f7d40d43c1
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 11:40:35 +0900

    add getall logic

--- api/servers.ts
+++ api/servers.ts
@@ -1,7 +1,10 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Firestore, collection, getDocs } from 'firebase/firestore/lite';
+import { createAppContainer, getFirestore } from "../internal/firebaseAppFactory";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  return res.json({
-    message: "This is the /servers endpoint!"
-  });
+  const db: Firestore = getFirestore(createAppContainer());
+  const snapshot = await getDocs(collection(db, "servers"));
+  const serverList = snapshot.docs.map(doc => doc.data());
+  return res.json(serverList);
 }
