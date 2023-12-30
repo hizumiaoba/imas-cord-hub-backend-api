94b0ff889eb0b54b210b6e0b04725c6a9d1b27e6
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 15:18:04 +0900

    fix?

--- api/servers.ts
+++ api/servers.ts
@@ -4,5 +4,5 @@ import clientPromise from "../_utils/mongo";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const client = await clientPromise;
   const db = client.db("imascordhubServer");
-  return res.status(200).json(db.collection("serverData"));
+  return res.status(200).json(db.collection("serverData").find({}));
 }
