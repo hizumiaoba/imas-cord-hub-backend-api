2b8f527abc0a318ffb78d6c5b817c6c7256798bf
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 15:32:05 +0900

    add type for exporting data

--- api/servers.ts
+++ api/servers.ts
@@ -6,5 +6,26 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const db = client.db("imascordhubServer");
   const collection = db.collection("serverData");
   const servers = await collection.find({}).toArray()
-  return res.status(200).json(servers);
+  const result: Array<exportType> = servers.map((server) => {
+    return {
+      id: server.id,
+      name: server.name,
+      ip: server.ip,
+      waifu: server.waifu,
+      description: server.description,
+      invite: server.invite,
+      splash: server.splash
+    }
+  });
+  return res.status(200).json(result);
+}
+
+type exportType = {
+  id: string,
+  name: string,
+  ip: string,
+  waifu: Array<String>,
+  description: string,
+  invite: string,
+  splash: string
 }
