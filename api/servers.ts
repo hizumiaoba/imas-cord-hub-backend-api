c078621a7c265202092d20a4fa26c5aa35130354
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 11:45:07 +0900

    extract logic

--- api/servers.ts
+++ api/servers.ts
@@ -4,7 +4,13 @@ import { createAppContainer, getFirestore } from "../internal/firebaseAppFactory
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const db: Firestore = getFirestore(createAppContainer());
-  const snapshot = await getDocs(collection(db, "servers"));
-  const serverList = snapshot.docs.map(doc => doc.data());
-  return res.json(serverList);
+  const servers = await getServers(db);
+  return res.json(servers);
+}
+
+const getServers = async (db: Firestore): Promise<Array<any>> => {
+  const serverCollection = collection(db, "servers");
+  const serverSnapshot = await getDocs(serverCollection);
+  const serverList = serverSnapshot.docs.map(doc => doc.data());
+  return serverList;
 }
