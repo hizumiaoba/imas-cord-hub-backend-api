88a17ff801805e14c34c367a275aab2d3a8a5962
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-30 12:02:49 +0900

    combine logic

--- api/servers.ts
+++ api/servers.ts
@@ -1,16 +1,13 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { Firestore, collection, getDocs } from 'firebase/firestore/lite';
 import { createAppContainer, getFirestore } from "./internal/firebaseAppFactory";
+import { FirebaseApp } from "firebase/app";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const db: Firestore = getFirestore(createAppContainer());
-  const servers = await getServers(db);
-  return res.json(servers);
-}
-
-const getServers = async (db: Firestore): Promise<Array<any>> => {
+  const firebaseApp: FirebaseApp = createAppContainer();
+  const db: Firestore = getFirestore(firebaseApp);
   const serverCollection = collection(db, "servers");
   const serverSnapshot = await getDocs(serverCollection);
-  const serverList = serverSnapshot.docs.map(doc => doc.data());
-  return serverList;
+  const servers = serverSnapshot.docs.map(doc => doc.data());
+  return res.json(servers);
 }
