2d564223ceb7cf0914d55975227417551769476b
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-01-17 16:07:40 +0900

    Add POST endpoint to health API for prototype

--- api/health.ts
+++ api/health.ts
@@ -1,5 +1,17 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export async function GET(req: VercelRequest, res: VercelResponse) {
   return res.status(200).json({ status: 'ok' });
+}
+
+export async function POST(req: VercelRequest, res: VercelResponse) {
+  if(req.headers['content-type'] !== 'application/json') {
+    return res.status(400).json({ error: 'invalid content type of body.'})
+  }
+  const body = req.body;
+  res.setHeader('Content-Type', 'application/json');
+  return res.status(200).json({
+    status: 'ok',
+    message: `Hello ${body.name}!`
+  });
 }
