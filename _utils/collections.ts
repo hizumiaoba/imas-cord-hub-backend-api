ebcf2c16ad60c8d5916f8f3c5c80e770ba8ea45d
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2024-01-24 14:56:44 +0900

    add util method for randomize arrays

---
+++ _utils/collections.ts
@@ -0,0 +1,16 @@
+export const shuffleArray = <T>(array: T[]): T[] => {
+    let currentIndex = array.length;
+    let temporaryValue: T;
+    let randomIndex: number;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
